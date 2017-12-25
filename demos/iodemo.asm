1627292f700c855e675c32db81b8b786a410c18c
Travor Liu <travor_lzh@outlook.com>
2017-12-25 08:04:22 +0800

    Not included in the OS
    It's an individual bootloader I created for a demonstration of I/O port.

--- demos/iodemo.asm
+++ demos/iodemo.asm
@@ -4,7 +4,7 @@
 	mov	al,'A'		; Print char A on the screen
 	int	10h			; Call BIOS
 
-	mov	al,0x0F		; Now we are going to move our cursor to the
+	mov	al,0x0E		; Now we are going to move our cursor to the
 	mov	dx,0x3D4	; top left of the screen without BIOS.
 	out	dx,al		; At first, tell the control I/O port to set
 					; the higher byte of the cursor offset
@@ -13,7 +13,7 @@
 	out	dx,al
 
 	mov	dx,0x3D4	; Now we will tell the control I/O port to
-	mov	al,0x0E		; set the lower byte of the cursor offset
+	mov	al,0x0F		; set the lower byte of the cursor offset
 	out	dx,al
 	mov	al,0		; Set the value to data I/O port
 	mov	dx,0x3D5
