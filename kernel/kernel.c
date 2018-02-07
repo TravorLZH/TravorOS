e183c257e365a5a0c8c19b5910113ffff3237e1c
Travor Liu <travor_lzh@outlook.com>
2018-02-07 10:11:11 -0500

    Working on interrupts

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -30,25 +30,5 @@ void _sleep(long long sec){
 }
 
 int main(void){
-	char cmdline[3];
-	int num;
-	unsigned long number;
-	char result;
-	while(1){
-		number=rand()%100;
-		printf("\nI'm thinking of a number between 0 and 100, try to guess what it is: ");
-		while(1){
-			memset(cmdline,0,3);
-			gets(cmdline);
-			num=atoi(cmdline);
-			result=_cmp(num,number);
-			if(result==2){printf("%d is Too large! ",num);continue;}
-			if(result==0){printf("%d is Too small! ",num);continue;}
-			if(result==1){break;}
-			printf("Oh fuck!\n");
-		}
-		printf("Oh! you guess it right! It's %d\n",num);
-	}
-	printf("Oh fuck! How do you get here?\n");
-	return 0;
+	printf("Hello world!\n");
 }
