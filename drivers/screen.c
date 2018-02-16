9f8160a0d4cc1e58e5b1f05453da418e198c0c8f
Travor Liu <travor_lzh@outlook.com>
2018-02-16 07:08:22 -0500

    Adjusted include directories

--- drivers/screen.c
+++ drivers/screen.c
@@ -15,7 +15,8 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-#include <io.h>
+#include <asm/ioports.h>
+#include <drivers/screen.h>
 
 // Implementations for screen operations
 
