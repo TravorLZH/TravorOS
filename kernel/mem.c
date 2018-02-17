935fe6ff2309f4658f0074739f730767e1c3532d
Travor Liu <travor_lzh@outlook.com>
2018-02-17 08:43:13 -0500

    Readded memory allocation

---
+++ kernel/mem.c
@@ -0,0 +1,36 @@
+#include <kernel/mem.h>
+#include <errno.h>
+
+unsigned char* mem_ptr; // Dynamic Memory started
+
+void kmem_init(size_t offset){
+	mem_ptr=(unsigned char*)offset;
+	// TODO: First unallocated memory block
+	*mem_ptr=0;	// BYTE: 0: allocated, 1: unallocated
+	*((size_t*)mem_ptr+4)=0x1000000; // DWORD: 2GB unallocated
+	*((size_t*)mem_ptr+8)=(size_t)mem_ptr;	// Next block's address;
+}
+void *kmalloc(size_t size){
+	char* offset=mem_ptr;
+	char* next=offset;
+	// If the size of the block is less than requirement or the block is allocated, enter the loop
+	while(*offset==1 || SIZE_OFFSET(offset)<size){
+		offset=(char*)NEXT_OFFSET(offset);	// goto the next block
+	}
+	// Set up an allocated block
+	*offset=1;					// Identify it's allocated
+	SIZE_OFFSET(offset)=size;
+	if(NEXT_OFFSET(offset)==NEXT_OFFSET(offset)){
+		NEXT_OFFSET(offset)=(size_t)offset+size+4;	// next unallocated block's address
+		next=(unsigned char*)NEXT_OFFSET(offset);	// Pointer to next
+		*next=0;				// Unallocated;
+		SIZE_OFFSET(next)=0x1000000-(size_t)next;	// Size of unallocated;
+		NEXT_OFFSET(next)=(size_t)next;		// Next unallocated block's address
+	}
+	return offset+9;
+}
+void kfree(void* ptr){
+	unsigned char* offset=OFFSET_BLOCK(ptr);
+	*offset=0;	// Unallocate the memory;
+}
+
