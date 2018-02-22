39cbb1496365741e9ff94521fedfa2b1d68c9da0
Travor Liu <travor_lzh@outlook.com>
2018-02-22 16:46:46 -0500

    Removed unnecessary files

--- demos/build_diskdemo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-yasm -fbin diskdemo.asm -o diskdemo.bin
-qemu-system-i386 -fda diskdemo.bin -hda disk.img
-
--- demos/video.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-	char* vidmem=(char*)0xb8000;
-	*vidmem='X';
-	*(vidmem+1)=0x0F;
-	return 0;
-}
