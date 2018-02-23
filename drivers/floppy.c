abd76403c18aeeb5d3bdd425bd2faa4e8e5c9060
Travor Liu <travor_lzh@outlook.com>
2018-02-23 13:32:27 -0500

    Added init_floppy()

--- drivers/floppy.c
+++ drivers/floppy.c
@@ -1,1 +1,11 @@
+/* floppy.c: A stub for my floppy driver */
 #include <drivers/floppy.h>
+#include <cpu/isr.h>
+
+static void floppy_handler(registers_t reg){
+  printf("Floppy IRQ received!\n");
+}
+
+void init_floppy(void){
+  register_interrupt_handler(IRQ6,floppy_handler);
+}
--- include/drivers/floppy.h
+++ include/drivers/floppy.h
@@ -7,6 +7,7 @@ extern	"C"{
 extern void motor_on();
 extern void motor_off();
 // TODO: Implement these functions
+extern void init_floppy(void);
 extern size_t read_sectors(size_t drive_no,size_t sectors,size_t start,char* addr);
 extern size_t write_sectors(size_t drive_no,size_t sectors,size_t start,char* addr);
 #ifdef	__cplusplus
