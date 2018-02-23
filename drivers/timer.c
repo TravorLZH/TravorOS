46a5ea968c357b25c1344a913e6485a4b2bba9c9
Travor Liu <travor_lzh@outlook.com>
2018-02-23 12:09:23 -0500

    Prettified output

--- drivers/timer.c
+++ drivers/timer.c
@@ -2,13 +2,16 @@
 #include <cpu/isr.h>
 #include <kernel/utils.h>
 #include <asm/ioports.h>
+#include <drivers/screen.h>
 
 size_t tick=0;
 
 static void timer_callback(registers_t regs){
 	tick++;
 	if(tick%20)return;
-	kprint("Time: %d\n",(int)tick/20);
+	char msg[BUFSIZ];
+	sprintf(msg,"Seconds: %d",(int)tick/20);
+	print_at(msg,(MAX_COLS-strlen(msg))/2,MAX_ROWS/2,0xF0);
 }
 
 void init_timer(void){
