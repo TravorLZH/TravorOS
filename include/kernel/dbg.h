ad2e57d35288fbe53ebb4a9df770a67001a82ec2
Travor Liu <travor_lzh@outlook.com>
2018-02-25 17:13:48 -0500

    kassert -> assert

--- include/kernel/dbg.h
+++ include/kernel/dbg.h
@@ -5,12 +5,11 @@
 #ifdef	__cplusplus
 extern	"C"{
 #endif
-#define	kassert(expr)	(void)((expr) || (__assert(#expr,__FILE__,__LINE__),0))
+#define	assert(expr)	(void)((expr) || (__assert(#expr,__FILE__,__LINE__),0))
 #define ktrace(...)	kprint("TRACING: " __VA_ARGS__)
 extern void __assert(const char* msg,const char* file, int line);
 #else
-#define	kassert(expr)
+#define	assert(expr)
 #define	ktrace(...)
 #endif
 #endif
-
