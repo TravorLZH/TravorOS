6009bc95ffe106e6fe546e0765bd37e516c41986
Travor Liu <travor_lzh@outlook.com>
2018-02-26 11:43:08 -0500

    inw and outw

--- lib/ioports.c
+++ lib/ioports.c
@@ -17,3 +17,13 @@ uint8_t inb(uint16_t port)
 			: "Nd"(port) );
 	return ret;
 }
+
+void outw(uint16_t port, uint16_t val){
+	asm volatile ( "outw %0, %1" : : "a"(val),"Nd"(port));
+}
+
+uint16_t inw(uint16_t port){
+	uint16_t ret;
+	asm volatile ("inw %1,%0" : "=a"(ret) : "Nd" (port));
+	return ret;
+}
