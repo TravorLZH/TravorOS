323f3caeeba1a3bfe95b427b66bf8f34e3c78d8a
Travor Liu <travor_lzh@outlook.com>
2018-02-26 11:38:36 -0500

    Added reboot and shutdown stub

---
+++ kernel/shutdown.asm
@@ -0,0 +1,28 @@
+%ifdef	MINGW
+%define	print_at _print_at
+%endif
+[global	shutdown]
+[global	_shutdown]
+[global	reboot]
+[global	_reboot]
+[extern	print_at]
+
+_shutdown:
+shutdown:
+	push	0x0F
+	push	-1
+	push	-1
+	push	NO_SHUTDOWN
+	call	print_at
+	add		esp,16
+	ret
+
+NO_SHUTDOWN	db	"shutdown: function not implemented!",0xA,0
+
+_reboot:
+reboot:
+	cli			; Disable interrupts
+	mov	al,0xFE	; Tell the keyboard controller to
+	out	0x64,al	; activate the CPU reset line
+	hlt
+	ret
