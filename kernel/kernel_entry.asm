2c7ff9af83b0148b0779407269fd2ea557ab14ba
Travor Liu <travor_lzh@outlook.com>
2018-02-28 18:36:03 -0500

    Removed useless function

--- kernel/kernel_entry.asm
+++ kernel/kernel_entry.asm
@@ -1,42 +1,16 @@
 %ifdef MINGW
 %define main _main
+%define	print_char	_print_char
 %endif
 ; Ensures that we jump straight into the kernel's entry function.
 [bits	32]
 [extern	main]	; Declare that we will be referencing the external symbol 'kernel_main'
-[global _cmp]
-[global __cmp]
+[extern print_char]
+push	0x0F
+push	-1
+push	-1
+push	0xA
+call	print_char
+add	esp,16
 call	main
 jmp	$
-
-; char _cmp(int a,int b): a simple comparison function
-; if a is greater than b, returns 1
-; if a is less than b, returns -1
-; if a is equal to b, returns 0
-__cmp:
-_cmp:
-	push	ebp
-	mov	ebp,esp
-	push	ebx
-	push	ecx
-	mov	ebx,[ebp+8]	; Get the first argument
-	mov	ecx,[ebp+12]	; Get the second argument
-	cmp	ebx,ecx
-	jg	.greater
-	cmp	ebx,ecx
-	je	.equal
-	cmp	ebx,ecx
-	jl	.less
-	jmp	.end
-.greater:
-	mov	eax,2
-	jmp	.end
-.equal:
-	mov	eax,1
-	jmp	.end
-.less:
-	mov	eax,0
-	jmp	.end
-.end:	leave
-	ret
-
