893914a10621738c52359fa42b242fcdb16180f8
Travor Liu <travor_lzh@outlook.com>
2018-02-28 15:27:17 -0500

    RTC header

---
+++ include/cpu/clock.h
@@ -0,0 +1,13 @@
+#include <def.h>
+#ifndef	__CLOCK_H_
+#define	__CLOCK_H_
+#ifdef	__cplusplus
+extern	"C"{
+#endif
+// TODO: Implement these functions
+extern size_t get_mills(void);
+extern void init_clock(void);
+#ifdef	__cplusplus
+}
+#endif
+#endif
