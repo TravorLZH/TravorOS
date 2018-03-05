02918e29e1c1c12615135d8f88c15eb1fa9e2dcd
Travor Liu <travor_lzh@outlook.com>
2018-03-05 15:21:24 -0500

    Improved PANIC function

--- include/kernel/utils.h
+++ include/kernel/utils.h
@@ -1,6 +1,7 @@
 #ifndef	__UTILS_H_
 #define	__UTILS_H_
 #define	kprint(...)	__kprintf("[kernel]: " __VA_ARGS__)
+#define	PANIC(...)	kprint("Panic: " __VA_ARGS__);while(1)
 #ifdef	__cplusplus
 extern int __kprintf(const char* str,...);
 #endif
