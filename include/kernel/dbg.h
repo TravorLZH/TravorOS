b2d0c24182d7baf0268b7c1780778250294ab6d9
Travor Liu <travor_lzh@outlook.com>
2018-03-05 15:20:32 -0500

    Tracing using function name as title

--- include/kernel/dbg.h
+++ include/kernel/dbg.h
@@ -6,7 +6,7 @@
 extern	"C"{
 #endif
 #define	assert(expr)	(void)((expr) || (__assert(#expr,__FILE__,__LINE__),0))
-#define ktrace(...)	kprint("TRACING: " __VA_ARGS__)
+#define ktrace(...)	kprint("%s: ",__func__);__kprintf(__VA_ARGS__)
 extern void __assert(const char* msg,const char* file, int line);
 #else
 #define	assert(expr)
