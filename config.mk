e7e3bbf684aa973303627bbb7b7f3013614b3650
Travor Liu <travor_lzh@outlook.com>
2018-03-16 09:37:23 -0400

    Little stuff

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 AS=yasm
-CC=i386-elf-gcc
-LD=i386-elf-ld
+CC=gcc
+LD=ld
 drivers_TARGETS=int.elf disk.elf time.elf stdio.elf gdt.elf
 %.o:	%.c
 	${CC} -Wno-implicit-function-declaration -g -ffreestanding -nostdlib -m32 -c $< -o $@ $(INCLUDE_DIR)
