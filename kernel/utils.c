7e274fcac00bc38fd30964e650bc571e8575d7a1
Travor Liu <travor_lzh@outlook.com>
2018-03-19 12:56:23 -0400

    __kprintf -> kprintf

--- kernel/utils.c
+++ kernel/utils.c
@@ -3,12 +3,18 @@
 #include <kernel/utils.h>
 #include <drivers/screen.h>
 
-int __kprintf(const char* str,...){
+static char color=0x0F;
+
+void kprint_set_color(char attr){
+	color=attr;
+}
+
+int kprintf(const char* str,...){
 	va_list vlist;
 	va_start(vlist,str);
 	char s[BUFSIZ];
 	int ret=vsprintf(s,str,vlist);
-	print_at(s,-1,-1,0x0F);
+	print_at(s,-1,-1,color);
 	va_end(vlist);
 	return ret;
 }
