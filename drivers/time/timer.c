6e3af9f7f26d3e34aa2dc4d165d2b19e649a814e
Travor Liu <travor_lzh@outlook.com>
2018-03-22 20:22:15 -0400

    Added frequency

--- drivers/time/timer.c
+++ drivers/time/timer.c
@@ -15,7 +15,11 @@ static void timer_callback(registers_t regs){
 	if(tick>0)tick--;
 }
 
-void init_timer(void){
-	kprint("Initializing timer...\n");
+void init_timer(size_t frequency){
+	kprint("Initializing timer with frequency %u Hz...",frequency);
 	register_interrupt_handler(IRQ0,timer_callback);
+	size_t divisor=1193180 / frequency;
+	outb(0x40,divisor & 0xFF);
+	outb(0x40,(divisor >> 8) & 0xFF);
+	print_at("[OK]\n",-1,-1,0x02);
 }
