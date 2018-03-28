78b9616e063e9a365d1ec76a71122472eac4f4a1
Travor Liu <travor_lzh@outlook.com>
2018-03-27 21:06:49 -0400

    [DRIVER] [FLOPPY] Working on floppy disk driver
    Though it's an obsolete technology

--- drivers/disk/floppy.c
+++ drivers/disk/floppy.c
@@ -9,10 +9,38 @@
 
 volatile char irq_received=0;
 
-char st0=0;
-char cylinder=0;
+uint8_t st0=0;
+uint8_t cy1=0;
+uint8_t current_drive=0;
 
 floppy_parameters floppy_disk;
+
+void flp_init_dma(void){
+	outb(0x0A,0x06);	// Mask DMA channel 2
+	outb(0xD8,0xFF);	// Reset master flip-flop
+	outb(0x04,0x00);	// Address=0x1000
+	outb(0x04,0x10);
+	outb(0xD8,0xFF);	// Reset master flip-flop
+	outb(0x05,0xFF);	// Count to 0x23FF (bytes in 3.5" floppy disk track
+	outb(0x80,0x00);	// External page register = 0
+	outb(0x0A,0x02);	// Unmask DMA channel 2
+}
+
+// Prepare DMA for read
+void flp_dma_read(void){
+	outb(0x0A,0x06);	// Mask DMA channel 2
+	outb(0x0B,0x56);	// Single transfer, address increment,
+				// autoinit, read, channel 2
+	outb(0x0A,0x02);	// Unmask DMA channel 2
+}
+
+// Prepare DMA for write
+void flp_dma_write(void){
+	outb(0x0A,0x06);
+	outb(0x0B,0x5A);	// Single transfer, address increment,
+				// autoinit, write, channel 2
+	outb(0x0A,0x02);
+}
 
 void flp_wait_irq(void){
 	while(irq_received==0);
@@ -20,88 +48,65 @@ void flp_wait_irq(void){
 	return;
 }
 
+void flp_write_dor(uint8_t val){
+	outb(FLP_DOR,val);
+}
+
+void flp_read_status(void){
+	return inb(FLP_MSR);
+}
+
+void flp_send_command(uint8_t cmd){
+	int i;
+	// Wait until data register is ready.
+	for(i=0;i<500;i++){
+		if(flp_read_status() & FLP_DATAREG){
+			outb(FLP_FIFO,command);
+			break;
+		}
+	}
+}
+
+void flp_write_ccr(uint8_t val){
+	outb(FLP_CTRL,val);
+}
+
+uint8_t flp_read_data(void){
+	int i;
+	for(i=0;i<500;i++){
+		if(flp_read_status() & FLP_DATAREG){
+			return inb(FLP_FIFO);
+		}
+	}
+}
+
 void floppy_handler(registers_t reg){
 	irq_received=1;
 }
 
-void init_floppy(void){
-	kprint("Initializing floppy driver...");
-	register_interrupt_handler(IRQ6,floppy_handler);
-	memcpy(&floppy_disk,DISK_PARAMETER_ADDRESS,sizeof(floppy_parameters));
-	print_at("[OK]\n",-1,-1,0x2);
-	flp_reset_controller(FLP_PRIMARY_BASE,0);
-}
-
-void flp_check_intstatus(int base,uint8_t *st0,uint8_t *cylinder){
-	flp_write_commmand(base,FLOPPY_INTERRUPT_STATUS);
-	flp_wait_data(base);
-	*st0=inb(base+FLP_DATA_REGISTER);
-	flp_wait_data(base);
-	*cylinder=inb(base+FLP_DATA_REGISTER);
-	ktrace("st0: %u (0x%x), cylinder: %u (0x%x)\n",st0,st0,cylinder,cylinder);
-	return;
-}
-
-void flp_wait_data(int base){
-	// Read main status register of controller and wait until
-	// the data is ready in the data register
-	while(((inb(base+FLP_MAIN_STATUS_REG))&0xD0)!=0xD0);
-	return;
-}
-
-void flp_write_commmand(int base,char command){
-	outb(base+FLP_DATA_REGISTER,command);
-}
-
-void flp_disable_controller(int base){
-	outb(base+FLP_DIGITAL_OUTPUT_REG,0x00);
-}
-
-void flp_enable_controller(int base){
-	outb(base+FLP_DIGITAL_OUTPUT_REG,0x0C);
-}
-
-void flp_reset_controller(int base,char drive){
-	flp_disable_controller(base);
-	flp_enable_controller(base);
+void flp_read_sector_imp(uint8_t head,uint8_t track,uint8_t sector){
+	flp_dma_read();
+	flp_send_command(FLP_READ_SECT|FLP_EXT_MULTITRACK|FLP_EXT_SKIP|FLP_EXT_DENSITY);
+	flp_send_command(head << 2 | current_drive);
+	flp_send_command(track);
+	flp_send_command(head);
+	flp_send_command(sector);
+	flp_send_command(FLP_SECTOR_DTL_512);
 	flp_wait_irq();
-	flp_check_intstatus(base, &st0, &cylinder);
-	outb(base+FLP_CONFIG_CONTROL_REG,0);
-	flp_configure_drive(base,drive);
-	flp_calibrate_drive(base,drive);
+	// Read status info
+	int j;
+	for(j=0;j<7;j++){
+		flp_read_data();
+	}
+	flp_check_int(&st0,&cy1);
 }
 
-void flp_calibrate_drive(int base,char drive){
-	flp_motor_on(base);
-	flp_write_commmand(base,FLOPPY_CALIBRATE_DRIVE);
-	flp_write_commmand(base,drive);
-	flp_wait_irq();
-	flp_check_intstatus(base,&st0,&cylinder);
+void flp_check_drive_data(size_t stepr,size_t loadt,size_t unloadt,uint8_t dma){
+	size_t data=0;
+	flp_send_command(FLP_SPECIFY);
+	data=((stepr & 0x0F) << 4) | unloadt & 0xF);
+	flp_send_command(data);
+	data=(loadt) << 1 | (dma==1)?1:0;
+	flp_send_command(data);
 }
 
-void flp_motor_on(int base){
-	outb(base+FLP_DIGITAL_OUTPUT_REG,0b00011100);
-	delay(5);
-}
-void flp_motor_off(int base){
-	outb(base+FLP_DIGITAL_OUTPUT_REG,0);
-	delay(5);
-}
-
-void flp_configure_drive(int base,char drive){
-	flp_write_commmand(base,FLOPPY_FIX_DRIVE_DATA);
-	flp_write_commmand(base,floppy_disk.steprate_headunload);
-	flp_write_commmand(base,floppy_disk.headload_ndma);
-}
-
-void flp_seek_track(uint8_t head,uint8_t cyl,uint8_t drive){
-	flp_write_commmand(FLP_PRIMARY_BASE,FLOPPY_SEEK_TRACK);
-	flp_write_commmand(FLP_PRIMARY_BASE,((head & 1) << 2) + drive);
-	flp_write_commmand(FLP_PRIMARY_BASE,cyl);
-	flp_wait_irq();
-	flp_check_intstatus(FLP_PRIMARY_BASE,&st0,&cylinder);
-}
-
-void flp_read_sector(uint8_t sector,uint8_t head,uint8_t cylinder,uint8_t drive,char* buffer){
-	flp_seek_track(head,cylinder,drive);
-}
--- include/drivers/floppy.h
+++ include/drivers/floppy.h
@@ -1,69 +1,64 @@
 #include <def.h>
 #ifndef	__FLOPPY_H_
 #define	__FLOPPY_H_
-// Base address for the primary and secondary floppydrive controllers
-// Drive A is usually on the primary one.
-#define	FLP_PRIMARY_BASE			0x03F0
-#define	FLP_SECONDARY_BASE			0x0370
 // Floppy Disk Controller registers
-#define	FLP_STATUS_REG_A			0x0000	/* PS2 Systems */
-#define	FLP_STATUA_REG_B			0x0001	/* PS2 Systems */
-#define	FLP_DIGITAL_OUTPUT_REG		0x0002
-#define	FLP_MAIN_STATUS_REG			0x0004
-#define	FLP_DATA_RATE_SELECT_REG	0x0004	/* PS2 Systems */
-#define	FLP_DATA_REGISTER			0x0005
-#define	FLP_DIGITAL_INPUT_REG		0x0007	/* AT Systems */
-#define	FLP_CONFIG_CONTROL_REG		0x0007	/* AT Systems */
-#define	FLP_PRIMARY_RESULT_STATUS	0x0000
-#define	FLP_SECONDARY_RESULT_STATUS	0x0000
-// Flopp Disk Controllery Commands
-#define	FLOPPY_FIX_DRIVE_DATA	0x03
-#define	FLOPPY_DRIVE_STATUS		0x04
-#define	FLOPPY_CALIBRATE_DRIVE	0x07
-#define	FLOPPY_INTERRUPT_STATUS	0x08
-#define	FLOPPY_READ_SECTOR		0x66
-#define	FLOPPY_WRITE_SECTOR		0xC5
-#define	FLOPPY_SEEK_TRACK		0x0F
-#define	FLOPPY_READ_TRACK		0x42
-#define	FLOPPY_FORMAT_TRACK		0x4D
-#define	FLOPPY_READ_SECTOR_ID	0x4A
-#define	FLOPPY_WRITE_DELETE_SECTOR	0xC5
-#define	FLOPPY_READ_DELETE_SECTOR	0xCC
-
-#define	DISK_PARAMETER_ADDRESS	0x00FEFC7	/* Location where disk parameters
-											* is store by BIOS */
-typedef struct{
-	uint8_t steprate_headunload;
-	uint8_t headload_ndma;
-	uint8_t motor_delay_off;	/* Specified in clock ticks */
-	uint8_t bytes_per_sector;
-	uint8_t sectors_per_track;
-	uint8_t gap_length;
-	uint8_t data_length;
-	uint8_t format_gap_length;
-	uint8_t filler;
-	uint8_t head_settle_time;	/* Specified in milliseconds */
-	uint8_t motor_start_time;	/* Specified in 1/8 seconds */
-} __attribute__((packed)) floppy_parameters;
-
+#define	FLP_DOR	0x3F2
+#define	FLP_MSR	0x3F4
+#define	FLP_FIFO	0x3F5	// Data register
+#define	FLP_CTRL	0x3F7
+// FDC DOR Masks
+#define	DOR_DRIVE0	0x00
+#define	DOR_DRIVE1	0x01
+#define	DOR_DRIVE2	0x02
+#define	DOR_DRIVE3	0x03
+#define	DOR_RESET	0x04
+#define	DOR_DRIVE0_MOTOR	0x10
+#define	DOR_DRIVE1_MOTOR	0x20
+#define	DOR_DRIVE2_MOTOR	0x40
+#define	DOR_DRIVE3_MOTOR	0x80
+// FDC MSR Masks
+#define	MSR_DRIVE0_POS_MODE	0x01
+#define	MSR_DRIVE1_POS_MODE	0x02
+#define	MSR_DRIVE2_POS_MODE	0x04
+#define	MSR_DRIVE3_POS_MODE	0x08
+#define	MSR_BUSY	0x10
+#define	MSR_DMA		0x20
+#define	MSR_DATAIO	0x40
+#define	MSR_DATAREG	0x80
+// FDC Commands
+#define	FLP_READ_TRACK	0x02
+#define	FLP_SPECIFY	0x03
+#define	FLP_CHECK_STAT	0x04
+#define	FLP_WRITE_SECT	0x05
+#define	FLP_READ_SECT	0x06
+#define	FLP_CALIBRATE	0x07
+#define	FLP_CHECK_INT	0x08
+#define	FLP_WRITE_DEL_S	0x09
+#define	FLP_WRITE_ID_S	0x0A
+#define	FLP_READ_DEL_S	0x0C
+#define	FLP_FORMAT_TRACK	0x0D
+#define	FLP_SEEK	0x0F
+// FDC Command Extension
+#define	FLP_EXT_SKIP	0x20
+#define	FLP_EXT_DENSITY	0x40
+#define	FLP_EXT_MULTITRACK	0x80
+// FDC Gap length
+#define	GAP3_STD	42
+#define	GAP3_5_14	32
+#define	GAP3_3_5	27
+// Size of sector: 2^n * 128
+#define	FLP_SECTOR_DTL_128	0
+#define	FLP_SECTOR_DTL_256	1
+#define	FLP_SECTOR_DTL_512	2
+#define	FLP_SECTOR_DTL_1024	4
 #ifdef	__cplusplus
 extern	"C"{
 #endif
 // TODO: Implement these functions
 extern void init_floppy(void);
 extern void flp_wait_irq(void);
-extern void flp_write_commmand(int base,char command);
-extern void flp_disable_controller(int base);
-extern void flp_enable_controller(int base);
-extern void flp_reset_controller(int base,char drive);
-extern void flp_check_intstatus(int base,uint8_t *st0,uint8_t *cylinder);
-extern void flp_configure_drive(int base,char drive);
-extern void flp_calibrate_drive(int base,char drive);
-extern void flp_wait_data(int base);
-extern void flp_motor_on(int base);
-extern void flp_motor_off(int base);
-extern size_t flp_read_sectors(size_t drive_no,size_t sectors,size_t start,char* addr);
-extern size_t flp_write_sectors(size_t drive_no,size_t sectors,size_t start,char* addr);
+extern void flp_write_dor(uint8_t val);
+extern uint8_t flp_read_status(void);
 #ifdef	__cplusplus
 }
 #endif
