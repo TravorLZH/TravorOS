41b2b00ea326682191bcfd90d98825b8fd0ede8c
Travor Liu <travor_lzh@outlook.com>
2018-03-28 14:19:44 -0400

    retrieve the old keyboard

--- drivers/stdio/kbd.c
+++ drivers/stdio/kbd.c
@@ -1,20 +1,20 @@
 /*
-* TravorOS: A simple OS running on Intel x86 Architecture
-* Copyright (C) 2017  Travor Liu
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ * TravorOS: A simple OS running on Intel x86 Architecture
+ * Copyright (C) 2017  Travor Liu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include <drivers/kbd.h>
 #include <drivers/screen.h>
 #include <asm/ioports.h>
@@ -28,7 +28,8 @@ static char ctrl=0;
 static char alt=0;
 static char shift=0;
 static char capital=0;
-static volatile char kb_interrupt=0;	// Make sure this is not erased by compiler
+static char kb_interrupt=0;
+static char special=0;
 // Table for scan code character mappings
 const char press_char[]={
 	VK_ESCAPE,'1','2','3','4','5','6','7','8','9','0','-','=',VK_BACK,
@@ -44,7 +45,7 @@ const char shift_char[]={
 	'|','Z','X','C','V','B','N','M','<','>','?',VK_SHIFT,'*',VK_MENU,
 	VK_SPACE,VK_CAPITAL,VK_F1,VK_F2,VK_F3,VK_F4,VK_F5,VK_F6,VK_F7,VK_F8,VK_F9,VK_F10
 };
-inline char code2char(unsigned char code){
+char code2char(unsigned char code){
 	char r;
 	if(shift==1){
 		r=shift_char[code-1];
@@ -70,48 +71,58 @@ char _getchar(){
 	}
 	return code2char(code);
 }
-inline char getScancode(){
-redo:
-	while(!kb_interrupt);
+char getScancode(){
+	/*char flag=port_byte_in(0x64);
+	  while(!(flag & 1)){
+	  flag=port_byte_in(0x64);
+	  }*/
+	while(!kb_interrupt || special);
 	kb_interrupt=0;
-	switch(inb(0x60)){
-		case VK_SPACE:case VK_MENU:case VK_CONTROL:case VK_SHIFT:
-		goto redo;
-	}
-	return inb(0x60);
+	return port_byte_in(0x60);
 }
 
 void keyboard_handler(registers_t r){
 	int x=port_byte_in(0x60);
+	char code;
 	kb_interrupt=1;
+	special=0;
 	if(x & 0x80){
 		outb(0x20,0x20);
-		switch(code2char(x)){
-			case VK_SHIFT:
+		// TODO: Handler when key is released.
+		code=code2char(x-0x80);
+		if(code==VK_SHIFT){
+			special=1;
 			shift=0;
-			break;
-			case VK_CONTROL:
+		}
+		if(code==VK_CONTROL){
+			special=1;
 			ctrl=0;
-			break;
-			case VK_MENU:
+		}
+		if(code==VK_MENU){
+			special=1;
 			alt=0;
-			break;
 		}
 	}else{
-		switch(code2char(x)){
-			case VK_SHIFT:
+		code=code2char(x);
+		if(code==VK_SHIFT){
+			special=1;
 			shift=1;
-			break;
-			case VK_CONTROL:
-			ctrl=1;
-			break;
-			case VK_MENU:
-			alt=1;
-			break;
-			case VK_CAPITAL:
-			capital=capital==1 ? 0 : 1;
-			break;
+			return;
 		}
+		if(code==VK_CONTROL){
+			special=1;
+			ctrl=1;
+		}
+		if(code==VK_MENU){
+			special=1;
+			alt=1;
+		}
+		if(code==VK_CAPITAL){
+			special=1;
+			capital=capital==1 ? 0 : 1;
+			return;
+		}
+		//putchar(code);
 	}
 }
 
@@ -120,3 +131,4 @@ void init_keyboard(){
 	register_interrupt_handler(0x21,keyboard_handler);
 	print_at("[OK]\n",-1,-1,0x02);
 }
+
