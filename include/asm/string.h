a4954b38af6f4fcbcd9f76104b63f6f38a863b96
Travor Liu <travor_lzh@outlook.com>
2018-04-13 12:07:16 -0400

    [LIB] Enhanced string functions for x86

---
+++ include/asm/string.h
@@ -0,0 +1,57 @@
+/* asm/string.h: x86 alternative for string functions */
+#ifndef	__STRING_H_
+#define	__STRING_H_
+#include <stddef.h>
+#ifdef	__cplusplus
+static	"C"{
+#endif
+static inline int strlen(const char *s)
+{
+	int ret;
+	__asm__("xor	%%ecx,%%ecx\n\t"
+		"not	%%ecx\n\t"
+		"xor	%%al,%%al\n\t"
+		"cld\n\t"
+		"repne	scasb\n\t"
+		"not	%%ecx\n\t"
+		"dec	%%ecx\n\t":"=c"(ret):"D"(s));
+	return ret;
+}
+static inline char *strcpy(char *dest,const char *src)
+{
+	__asm__("cld\n"
+		"1:\tlodsb\n\t"
+		"stosb\n\t"
+		"test	%%al,%%al\n\t"
+		"jne	1b"
+		::"S"(src),"D"(dest):"esi","edi","eax");
+	return  dest;
+}
+static inline char *strncpy(char *dest,const char *src,int count){
+	__asm__("cld\n"
+		"1:\tdecl	%2\n\t"
+		"js	2f\n\t"
+		"lodsb\n\t"
+		"stosb\n\t"
+		"testb	%%al,%%al\n\t"
+		"jne	1b\n\t"
+		"rep	stosb\n\t"
+		"2:"
+		::"S"(src),"D"(dest),"c"(count):"esi","edi","eax","ecx");
+	return dest;
+}
+static inline void *memcpy(void *dest,const void *src,size_t count){
+	__asm__("cld\n"
+		"rep movsb"
+		::"S"(src),"D"(dest),"c"(count):"memory");
+	return dest;
+}
+static inline void *memset(void *dest,int c,size_t count){
+	__asm__("cld\n"
+		"rep stosb"
+		::"D"(dest),"a"(c),"c"(count):"memory");
+	return dest;
+}
+#ifdef	__cplusplus
+}
+#endif
