f3756f38b089d4d4861faf24291f379414d65a50
Travor Liu <travor_lzh@outlook.com>
2018-04-13 15:14:56 -0400

    [LIB] New wrapper for screen (using system calls)

---
+++ lib/iowrap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <kernel/syscall.h>
+#include <drivers/screen.h>
+
+int puts(const char *s){
+	sys_puts(s);
+	return 1;
+}
+
+int putchar(int c){
+	sys_putchar(c);
+	return c;
+}
