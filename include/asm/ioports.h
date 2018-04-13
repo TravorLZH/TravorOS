91097a6da7fde1c0d05b465298b0e95ed727527b
Travor Liu <travor_lzh@outlook.com>
2018-04-13 09:56:35 -0400

    Make io operation inline

--- include/asm/ioports.h
+++ include/asm/ioports.h
@@ -8,10 +8,31 @@
 #ifdef	__cplusplus
 extern	"C"{
 #endif
-extern uint8_t inb(uint16_t port);
-extern void outb(uint16_t port,uint8_t val);
-extern uint16_t inw(uint16_t port);
-extern void outw(uint16_t port,uint16_t val);
+static inline void outb(uint16_t port, uint8_t val)
+{
+	asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+}
+
+static inline uint8_t inb(uint16_t port)
+{
+	uint8_t ret;
+	asm volatile ( "inb %1, %0"
+			: "=a"(ret)
+			: "Nd"(port) );
+	return ret;
+}
+
+static inline void outw(uint16_t port, uint16_t val)
+{
+	asm volatile ( "outw %0, %1" : : "a"(val),"Nd"(port));
+}
+
+static inline uint16_t inw(uint16_t port)
+{
+	uint16_t ret;
+	asm volatile ("inw %1,%0" : "=a"(ret) : "Nd" (port));
+	return ret;
+}
 #ifdef	__cplusplus
 }
 #endif
