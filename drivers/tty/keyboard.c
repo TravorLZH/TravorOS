4be116c949ccd67a386db450f7fda1cbbedbedf7
Travor Liu <travor_lzh@outlook.com>
2018-04-13 15:19:02 -0400

    [DRIVERS] [TTY] Half done `keyboard'

--- drivers/tty/keyboard.c
+++ drivers/tty/keyboard.c
@@ -1,4 +1,4 @@
-#include <stddef.h>
+#include <def.h>
 #include <asm/string.h>
 #include <asm/ioports.h>
 #include <cpu/isr.h>
@@ -9,25 +9,52 @@
 
 #define	BACKSPACE	0x0E
 #define	ENTER		0x1C
+#define	CTRL		0x1D
+#define	LSHIFT		0x2A
+#define	RSHIFT		0x36
+
+#define	RELEASED_KEY(code)	((code) & 0x80)
 
 static char keyboard_buffer[BUFSIZ];
 static void (*flush_handler)(const char*)=NULL;
 
 #define	MAX_SCANCODE	57
 
-const char scancode="??1234567890-=??qwertyuiop[]??asdfghjkl;'`?\\zxcvbnm,./??? ";
+const char scancode_table[]="??1234567890-=??qwertyuiop[]??asdfghjkl;'`\?\\zxcvbnm,./??? ";
+
+struct {
+	char interrupt:1;
+	char ctrl:1;
+	char shift:1;
+	char caps:1;
+	char reserved:4;
+} __attribute__((packed)) kbd;
 
 void kbd_flush(void)
 {
-	flush_handler(keyboard_buffer);
+	if(flush_handler!=NULL){
+		flush_handler(keyboard_buffer);
+	}
 	memset(keyboard_buffer,0,BUFSIZ);
+}
+
+uint8_t kbd_read(void){
+	while(!kbd.interrupt);
+	kbd.interrupt=0;
+	return inb(0x60);
+}
+
+int getchar(void){
+	char ch=scancode_table[kbd_read()];
+	print_char(ch,-1,-1,0x07);
+	return ch;
 }
 
 static inline void backspace(void)
 {
 	int len=strlen(keyboard_buffer);
 	if(len>0){
-		buf[len-1]='\0';
+		keyboard_buffer[len-1]='\0';
 	}
 }
 
@@ -35,12 +62,13 @@ static inline void backspace(void)
 static inline void append(char c)
 {
 	int len=strlen(keyboard_buffer);
-	s[len]=c;
-	s[len+1]='\0';
+	keyboard_buffer[len]=c;
+	keyboard_buffer[len+1]='\0';
 }
 
 static void keyboard_callback(registers_t regs)
 {
+	kbd.interrupt=1;
 	uint8_t scancode=inb(0x60);
 	if(scancode>MAX_SCANCODE){
 		return;
@@ -53,7 +81,7 @@ static void keyboard_callback(registers_t regs)
 		kbd_flush();
 		break;
 	default:
-		append(scancode[code]);
+		append(scancode_table[scancode]);
 	}
 }
 
@@ -68,6 +96,5 @@ void kbd_flush_handler(void (*handler)(const char*),char override)
 void init_keyboard(void)
 {
 	kprint("Initializing keyboard...\n");
-	kbd_setbuf(keyboard_buffer);
 	register_interrupt_handler(IRQ1,keyboard_callback);
 }
--- include/drivers/keyboard.h
+++ include/drivers/keyboard.h
@@ -1,10 +1,13 @@
 /* keyboard.h: A substitution for old keyboard driver `kbd' */
 #ifndef	__KEYBOARD_H_
 #define	__KEYBOARD_H_
+#include <def.h>
 #ifdef	__cplusplus
 extern	"C"{
 #endif
 extern void init_keyboard(void);
+extern uint8_t kbd_read(void);
+extern int getchar(void);
 #ifdef	__cplusplus
 }
 #endif
