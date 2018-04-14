d7e133aac8222cd7563e508dc496671235f97bbd
Travor Liu <travor_lzh@outlook.com>
2018-04-14 18:32:56 -0400

    [LIB] Coding style

--- lib/iowrap.c
+++ lib/iowrap.c
@@ -2,12 +2,14 @@
 #include <kernel/syscall.h>
 #include <drivers/screen.h>
 
-int puts(const char *s){
+int puts(const char *s)
+{
 	sys_puts(s);
 	return 1;
 }
 
-int putchar(int c){
+int putchar(int c)
+{
 	sys_putchar(c);
 	return c;
 }
