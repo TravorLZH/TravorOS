ec0455d5fb4019236ffb8c4762184d0cd3b36c6a
Travor Liu <travor_lzh@outlook.com>
2018-04-17 18:58:35 -0400

    [LIB] Complete gets()

--- lib/iowrap.c
+++ lib/iowrap.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 #include <kernel/syscall.h>
 #include <drivers/screen.h>
+
+static char *userbuf=NULL;
+volatile char flushed=0;
 
 int puts(const char *s)
 {
@@ -12,4 +16,19 @@ int putchar(int c)
 {
 	sys_putchar(c);
 	return c;
+}
+
+static void my_flush_handler(const char *buf){
+	flushed=1;
+	if(userbuf!=NULL){
+		strcpy(userbuf,buf);
+	}
+}
+
+char *gets(char *s){
+	userbuf=s;
+	kbd_flush_handler(my_flush_handler);
+	while(!flushed);
+	flushed=0;
+	return userbuf;
 }
