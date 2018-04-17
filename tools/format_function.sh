6aaea2df84456932a29b887c4c1383755f6d3bfc
Travor Liu <travor_lzh@outlook.com>
2018-04-17 15:09:56 -0400

    [TOOLS] Fix working directory problem

--- tools/format_function.sh
+++ tools/format_function.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find . -name '*.c' -type f -exec sed -E -i 's/(^(\w+\s)+\w+\([^)]*\))\{/\1\n\{/g' {} \;
+find ${PWD} -name '*.c' -type f -exec sed -E -i 's/(^(\w+\s)+\w+\([^)]*\))\{/\1\n\{/g' {} \;
