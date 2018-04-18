542d9393362884ac524597281dcaaea6333f1a04
Travor Liu <travor_lzh@outlook.com>
2018-04-18 13:33:33 -0400

    [TOOLS] Also format header files

--- tools/format_function.sh
+++ tools/format_function.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 find ${PWD} -name '*.c' -type f -exec sed -E -i 's/(^(\w+\s)+\w+\([^)]*\))\{/\1\n\{/g' {} \;
+find ${PWD} -name '*.h' -type f -exec sed -E -i 's/(^(\w+\s)+\w+\([^)]*\))\{/\1\n\{/g' {} \;
