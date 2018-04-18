ef4b978a3b6e16350c359617476ca60e8ed4caa8
Travor Liu <travor_lzh@outlook.com>
2018-04-18 13:34:36 -0400

    [LIB] Make sti and cli inline

--- include/asm/interrupt.h
+++ include/asm/interrupt.h
@@ -1,10 +1,18 @@
 #ifndef	__INTERRUPT_H_
 #define	__INTERRUPT_H_
+#define	sti()	set_interrupt()
+#define	cli()	clear_interrupt()
 #ifdef	__cplusplus
 extern	"C"{
 #endif
-extern char set_interrupt(void);
-extern char clear_interrupt(void);
+static inline char set_interrupt(void)
+{
+	__asm__("sti");
+}
+static inline char clear_interrupt(void)
+{
+	__asm__("cli");
+}
 #ifdef	__cplusplus
 }
 #endif
