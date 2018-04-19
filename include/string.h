6b82828896bb9fd1ddeecb9089ef5bc3e7111824
Travor Liu <travor_lzh@outlook.com>
2018-04-19 17:49:32 -0400

    [LIB] Add pad_zero() declaration

--- include/string.h
+++ include/string.h
@@ -1,20 +1,3 @@
-/*
-* TravorOS: A simple OS running on Intel x86 Architecture
-* Copyright (C) 2017  Travor Liu
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
 /* string.h: String and Memory Functions */
 #include <def.h>
 #ifndef	__STRING_H_
@@ -31,6 +14,7 @@ extern char *strcpy(char* dest,const char* src);
 extern unsigned int strlen(const char*);
 /* Not standard string function(s) */
 extern char *strrev(char *str);
+extern char *pad_zero(int num,char *str);
 
 extern void *memcpy(void *dest,const void *src,size_t len);
 extern void *memmove(void *dest,const void *src,size_t len);
