a1839f6f0cb0deb5a20274cc5320ac1ee0bce98d
Travor Liu <travor_lzh@outlook.com>
2018-04-19 17:47:47 -0400

    [LIB] Working on zero padding

--- lib/printf.c
+++ lib/printf.c
@@ -1,23 +1,23 @@
-/*
-* TravorOS: A simple OS running on Intel x86 Architecture
-* Copyright (C) 2017  Travor Liu
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-/* vsprintf.c: Formating string using variable arguments */
+/* printf.c: Formating string using variable arguments */
+#include <kernel/utils.h>
 #include <stdarg.h>
+#include <string.h>
 #include <stdio.h>
+
+char *pad_zero(int num,char *str){
+	int len=strlen(str);
+	int diff=num-len;
+	if(diff<=0){
+		return str;
+	}
+	char zeroes[diff+1];
+	char tmp[num+1];
+	memset(zeroes,'0',diff);
+	zeroes[diff]='\0';
+	memset(tmp,0,len+1);
+	sprintf(tmp,"%s%s",zeroes,str);
+	strcpy(str,tmp);
+}
 
 // formatting string: The fundamental function of printf family.
 int vsprintf(char* buffer,const char *format,va_list vlist)
