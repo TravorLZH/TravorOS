28c99001c1793805f2bbcbab10591ed3397ae6b1
Travor Liu <travor_lzh@outlook.com>
2018-04-20 16:35:53 -0400

    [KERNEL] Change BSoD function definition

--- include/kernel/bsod.h
+++ include/kernel/bsod.h
@@ -1,10 +1,11 @@
-#include <def.h>
 #ifndef	__BSOD_H_
 #define	__BSOD_H_
+#include <def.h>
+#include <cpu/isr.h>
 #ifdef	__cplusplus
 extern	"C"{
 #endif
-extern void bsod_enter(size_t int_no,const char *name,size_t err_no,const char *description,size_t eip);
+extern void bsod_enter(const char *name,const char *description,registers_t *regs);
 #ifdef	__cplusplus
 }
 #endif
