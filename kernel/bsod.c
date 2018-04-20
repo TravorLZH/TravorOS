783735765494e31311ced5e9f9f14a9762fff24e
Travor Liu <travor_lzh@outlook.com>
2018-04-20 16:36:16 -0400

    [KERNEL] Dump all registers during blue screen

--- kernel/bsod.c
+++ kernel/bsod.c
@@ -3,17 +3,19 @@
 #include <drivers/screen.h>
 
 // Enter blue screen of death
-void bsod_enter(size_t int_no,const char *name,size_t err_code,const char *description,size_t eip)
+void bsod_enter(const char *name,const char *description,registers_t *regs)
 {
 	clear_screen(0x1F);
 	kprint_set_color(0x1F);
 	kprintf("An error has occured, and TravorOS has shutdown in order to prevent damage to your computer.\n\n");
 	kprintf("The only way to exit this screen is to reboot the machine\n");
 	kprintf("\nTechnical Information:\n\n");
-	kprintf("0x%x: %s\n\n",int_no,name);
-	kprintf("Error Code: 0x%x\n",err_code);
+	kprintf("0x%x: %s\n\n",regs->int_no,name);
+	kprintf("Error Code: 0x%x\n",regs->err_code);
 	kprintf("Description:\n%s",description);
-	kprintf("\n\nEIP=0x%x\n",eip);
+	kprintf("\n\nEAX=0x%x, EBX=0x%x, ECX=0x%x, EDX=0x%x\n"
+		"ESI=0x%x, EDI=0x%x, EBP=0x%x, ESP=0x%x",regs->eax,regs->ebx,regs->ecx,regs->edx,regs->esi,regs->edi);
+	kprintf("\n\nEIP=0x%x\n",regs->eip);
 	disable_cursor();
 	while(1);
 }
