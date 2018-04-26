d5ec865bb224757169a41c6eed2d2cbb245de70f
Travor Liu <travor_lzh@outlook.com>
2018-04-26 14:58:16 -0400

    [CPU] CPUID

---
+++ include/cpu/cpuid.h
@@ -0,0 +1,32 @@
+#ifndef	__CPUID_H_
+#define	__CPUID_H_
+#ifdef	__cplusplus
+extern	"C"{
+#endif
+enum cpuid_requests {
+	CPUID_VENDORSTRING,
+	CPUID_FEATURES,
+	CPUID_TLB,
+
+	CPUID_INTELEXTENDED=0x80000000,
+	CPUID_INTELFEATURES,
+	CPUID_INTELBRANDSTRING,
+	CPUID_INTELBRANDSTRINGMORE,
+	CPUID_INTELBRANDSTRINGEND
+};
+
+/* Single request to CPUID */
+static inline void cpuid(int code,size_t *a,size_t *d)
+{
+	asm volatile("cpuid":"=a"(*a),"=d"(*d):"a"(code):"ecx","ebx");
+}
+/* Issue a complete request, storing general registers output as a string */
+static inline int cpuid_string(int code,size_t where[4]){
+	int ret;
+	asm volatile("cpuid":"=a"(where[0]),"=b"(where[1]),"=c"(where[2]),"=d"(where[3]):"a"(code));
+	return ret;
+}
+#ifdef	__cplusplus
+}
+#endif
+#endif
