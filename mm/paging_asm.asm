fc263f92fecd9ad7079a991a1b303217555c16f8
Travor Liu <travor_lzh@outlook.com>
2018-05-21 16:20:40 -0400

    [MM] current_page_directory: get the CR3

--- mm/paging_asm.asm
+++ mm/paging_asm.asm
@@ -1,13 +1,24 @@
 [global load_page_directory]
 [global enable_paging]
+[global current_page_directory]
 
-; void load_page_directory(void *pgdir);
+; void load_page_directory(size_t *pgdir);
 ; Load the address of page directory to CR3
 load_page_directory:
 	push	ebp
 	mov	ebp,esp
 	mov	eax,[ebp+8]
 	mov	cr3,eax
+	pop	ebp
+	ret
+
+; size_t *current_page_directory(void);
+; Get the physical address of the
+; current page directory from CR3
+current_page_directory:
+	push	ebp
+	mov	ebp,esp
+	mov	eax,cr3
 	pop	ebp
 	ret
 
