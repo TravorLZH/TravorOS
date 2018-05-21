6c752696a4045c96b37b9730de75164323d37af8
Travor Liu <travor_lzh@outlook.com>
2018-05-21 15:16:53 -0400

    [MM] Implemented get_free_page, free_page, and alloc_page

--- mm/paging.c
+++ mm/paging.c
@@ -36,6 +36,13 @@ static void page_fault(registers_t regs)
 	bsod_enter("Page Fault",description,&regs);
 }
 
+size_t *current_page_directory(void)
+{
+	register size_t *ret;
+	__asm__("movl %%cr3,%0":"=r" (ret)::"eax");
+	return ret;
+}
+
 void init_paging(void)
 {
 	assert(sizeof(page_t) == 4);	// Make sure the page union is set up properly
@@ -44,7 +51,7 @@ void init_paging(void)
 	frame_bitset=(bitset_t*)kmalloc_a(bitset_size);
 	kernel_directory=(size_t*)kmalloc_a(1024*sizeof(size_t));
 	kernel_table=(page_t*)kmalloc_a(1024*sizeof(page_t));
-	kernel_heap=(page_t*)kmalloc_a(1024*sizeof(page_t));
+	kernel_heap=(page_t*)kmalloc_a(1024*sizeof(page_t));	// Space for placement malloc
 	liballoc_heap=(page_t*)kmalloc_a(1024*sizeof(page_t));
 	// TODO: Initialize the page directory
 	size_t i;
@@ -59,17 +66,16 @@ void init_paging(void)
 	}
 	/* Identity map the first 8MB of the physical memory */
 	for(i=0;i<1024;i++){
-		map_frame(kernel_table+i,i,0,1);
-		set_frame(i);
+		map_frame(kernel_table+i,(void*)(i*FRAME_SIZE),0,1);
+		set_frame((void*)(i*FRAME_SIZE));
 	}
 	for(i=0;i<1024;i++){
-		map_frame(kernel_heap+i,i+1024,0,1);
-		set_frame(i+1024);
+		map_frame(kernel_heap+i,(void*)((i+1024)*FRAME_SIZE),0,1);
+		set_frame((void*)((i+1024)*FRAME_SIZE));
 	}
 	for(i=0;i<1024;i++){
 		liballoc_heap[i].present=0;	// Make sure they are not present
 	}
-	kernel_table[3].val=3*0x1000;	// Create a not-present page
 	kernel_directory[0]=((size_t)kernel_table) | 3;		/* 0 - 4MB:  Kernel space */
 	kernel_directory[1]=((size_t)kernel_heap) | 3;		/* 4 - 8MB:  Kernel heap */
 	kernel_directory[2]=((size_t)liballoc_heap) | 3;	/* 8 - 12MB: liballoc heap */
@@ -81,24 +87,89 @@ void init_paging(void)
 
 page_t *get_page(void *address,char create,size_t *pgdir)
 {
+	if(pgdir==NULL){
+		pgdir=current_page_directory();
+	}
 	size_t addr=(size_t)address;
-	addr/=0x1000;
+	addr/=PAGE_SIZE;
+	size_t tmp;
 	size_t table_index=addr/1024;
 	if(pgdir[table_index]){
-		return (void*)(pgdir[table_index]+addr%1024);
+		tmp=pgdir[table_index];
+		tmp-=tmp%PAGE_SIZE;
+		return (page_t*)(tmp+addr%1024*sizeof(page_t));
 	}else if(create){
-		size_t tmp=(size_t)kmalloc_a(1024*sizeof(page_t));
-		memset((void*)tmp,0,0x1000);
+		ktrace("Page table not found, allocating...");
+		tmp=(size_t)kmalloc_a(1024*sizeof(page_t));
+		memset((void*)tmp,0,PAGE_SIZE);
 		pgdir[table_index]=tmp | 0x7;	// PRESENT, RW, US.
-		return (void*)(pgdir[table_index]+addr%1024);
+		return (page_t*)tmp+addr%1024;
 	}else{
 		return NULL;
 	}
 }
 
-/* This function helps me integrate liballoc into my kernel */
+void *alloc_page(void)
+{
+	void *addr=get_free_page();
+	page_t *page=get_page(addr,1,0,current_page_directory());
+	page->present=1;
+	return addr;
+}
+
+void *alloc_pages(int n)
+{
+	assert(n>0);
+	int i,j;
+	for(i=0;i<MAX_FRAMES;i++){
+}
+
+void free_page(void *address)
+{
+	size_t addr=(size_t)address;
+	addr/=PAGE_SIZE;
+	addr*=PAGE_SIZE;	// Make sure it's aligned
+	size_t *pgdir=current_page_directory();
+	page_t *pg=get_page(address,0,pgdir);
+	if(pg){
+		free_frame((void*)(pg->frame*FRAME_SIZE));
+		pg->present=0;
+	}
+}
+
+void free_pages(void *address,int n)
+{
+	assert(n>0);
+	size_t *pgdir=current_page_directory();
+	size_t addr=(size_t)address;
+	int i;
+	for(i=0;i<n;i++){
+		free_page((void*)addr);
+		addr+=PAGE_SIZE;
+	}
+}
+
+char is_free_page(void *address)
+{
+	size_t *pgdir=current_page_directory();
+	page_t *pg=get_page(address,1,pgdir);
+	return pg->present?0:1;
+}
+
 void *get_free_page(size_t *pgdir)
 {
-	size_t tb_index;
-	size_t pg_index;
+	if(pgdir==NULL){
+		pgdir=current_page_directory();
+		ktrace("Using current page directory at address 0x%x",pgdir);
+	}
+	size_t address;
+	page_t *pg=NULL;
+	for(address=0;address<MAPPED_SIZE;address+=PAGE_SIZE){
+		pg=get_page((void*)address,1,pgdir);
+		if(pg->present==0){
+			ktrace("Found free page at 0x%x",address);
+			return (void*)address;
+		}
+	}
+	return NULL;
 }
