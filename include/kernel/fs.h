62a25ced2de9f2d25a8f0fc73a91aaf3e1633a2c
Travor Liu <travor_lzh@outlook.com>
2018-05-22 07:58:06 -0400

    [FS] More declaration for filesystem

--- include/kernel/fs.h
+++ include/kernel/fs.h
@@ -11,6 +11,13 @@
 #define	FS_SYMLINK	0x06
 #define	FS_MOUNTPOINT	0x08
 
+/* Returned by readdir call */
+struct dirent {
+	char name[128];	// Filename
+	uint32_t ino;	// Inode number, conforming to POSIX
+};
+
+typedef struct fs_node fs_node_t;
 typedef uint32_t (*readfunc_t)(struct fs_node*,uint32_t,uint32_t,uint8_t*);
 typedef uint32_t (*writefunc_t)(struct fs_node*,uint32_t,uint32_t,uint8_t*);
 typedef void (*openfunc_t)(struct fs_node*);
@@ -18,14 +25,8 @@ typedef void (*closefunc_t)(struct fs_node*);
 typedef struct dirent * (*readdirfunc_t)(struct fs_node*,uint32_t);
 typedef struct fs_node * (*finddirfunc_t)(struct fs_node*,char *name);
 
-/* Returned by readdir call */
-struct dirent {
-	char name[128];	// Filename
-	uint32_t ino;	// Inode number, conforming to POSIX
-};
-
 /* Data structure of inode */
-typedef struct fs_node {
+struct fs_node {
 	char name[128];		// The filename
 	uint32_t mask;		// Permission bits
 	uint32_t uid;
@@ -40,17 +41,22 @@ typedef struct fs_node {
 	readdirfunc_t readdir;
 	finddirfunc_t finddir;
 	struct fs_node *ptr;	// Used by mountpoints and symlinks.
-} fs_node_t;
+};
 
-uint32_t read_fs(fs_node_t *node,uint32_t offset,uint32_t size,uint8_t *buffer);
-uint32_t write_fs(fs_node_t *node,uint32_t offset,uint32_t size,uint8_t *buffer);
-void open_fs(fs_node_t *node,uint8_t read,uint8_t write);
-void close_fs(fs_node_t *node);
-struct dirent *readdir_fs(fs_node_t *node,index);
 #ifdef	__cplusplus
 extern	"C"{
 #endif
-
+/* Read a file */
+extern uint32_t read_fs(fs_node_t *node,uint32_t offset,uint32_t size,uint8_t *buffer);
+/* Write a file */
+extern uint32_t write_fs(fs_node_t *node,uint32_t offset,uint32_t size,uint8_t *buffer);
+/* Open a file */
+extern void open_fs(fs_node_t *node,uint8_t read,uint8_t write);
+/* Close a file */
+extern void close_fs(fs_node_t *node);
+/* Read a directory */
+extern struct dirent *readdir_fs(fs_node_t *node,uint32_t index);
 #ifdef	__cplusplus
 }
+#endif
 #endif
